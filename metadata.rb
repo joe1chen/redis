2e362fa4e159fe9543543d11ea61b90797db396a
Joey Aghion <joey@aghion.com>
2014-01-29 17:04:56 -0500

    Declare default recipe in metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -6,3 +6,5 @@ description      "Installs/Configures redis"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1.0"
 depends          'build-essential', '~> 1.4.0'
+
+recipe 'redis::default', 'Install, configure, and run redis'
